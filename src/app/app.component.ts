54c23764a9264ebc50ef676aea0ce4908048c955
Kai Schulz <info@sparrowmedia.de>
2024-11-10 08:30:52 +0100

    added basket component, added some buttons with material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DatabaseComponent } from './database/database.component';
+import { DatabaseComponent } from './database/database.component'
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DatabaseComponent],
+  imports: [RouterOutlet, DatabaseComponent, MatButtonModule, MatIconModule, MatDividerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
